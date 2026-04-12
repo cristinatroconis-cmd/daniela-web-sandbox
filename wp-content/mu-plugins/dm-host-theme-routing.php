c38a1fa0e9ddc69a93d9ca2c925baa2d40f2c096
Cristina Troconis <cristinatroconis@gmail.com>
2026-04-11 21:54:48 -0400

    feat: Add DM - Host Theme Routing plugin for dynamic theme and site URL management

--- wp-content/mu-plugins/dm-host-theme-routing.php
+++ wp-content/mu-plugins/dm-host-theme-routing.php
@@ -33,12 +33,22 @@ $dm_live_host    = 'danielamontespsic.com';
 $dm_staging_host = 'v2vvroh9bv-staging.onrocket.site';
 
 if ($dm_host === $dm_staging_host) {
+    $dm_staging_base = 'https://' . $dm_staging_host;
+
     add_filter('pre_option_home', static function () use ($dm_staging_host) {
         return 'https://' . $dm_staging_host;
     });
 
     add_filter('pre_option_siteurl', static function () use ($dm_staging_host) {
         return 'https://' . $dm_staging_host;
+    });
+
+    add_filter('pre_option_whl_page', static function () {
+        return 'acceso';
+    });
+
+    add_filter('pre_option_whl_redirect_admin', static function () {
+        return 'acceso';
     });
 
     add_filter('pre_option_stylesheet', static function () {
@@ -48,6 +58,19 @@ if ($dm_host === $dm_staging_host) {
     add_filter('pre_option_template', static function () {
         return 'shoptimizer';
     });
+
+    // Safety net: keep all runtime redirects inside staging host.
+    add_filter('wp_redirect', static function ($location) use ($dm_staging_base, $dm_live_host) {
+        if (! is_string($location) || $location === '') {
+            return $location;
+        }
+
+        if (stripos($location, 'https://' . $dm_live_host) === 0 || stripos($location, 'http://' . $dm_live_host) === 0) {
+            return preg_replace('#^https?://(www\.)?danielamontespsic\.com#i', $dm_staging_base, $location);
+        }
+
+        return $location;
+    }, 9999);
 } elseif ($dm_host === $dm_live_host || $dm_host === 'www.' . $dm_live_host) {
     add_filter('pre_option_home', static function () use ($dm_live_host) {
         return 'https://' . $dm_live_host;
